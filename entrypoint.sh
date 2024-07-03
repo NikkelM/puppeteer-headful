f4e0cf2f61a12263fb07bfac696d6ed8ce540148
Nikkel Mollenhauer <57323886+NikkelM@users.noreply.github.com>
2024-07-03 18:21:13 +0100

    Error handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
+Xvfb -ac :99 -screen 0 1280x1024x16 > &
+# Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
+
+XVFB_PID=$!
+
+# Wait a brief moment to ensure Xvfb starts
+sleep 2
+
+# Check if Xvfb is running by checking if the PID exists
+if ! kill -0 $XVFB_PID > /dev/null 2>&1; then
+    echo "Xvfb failed to start"
+    exit 1
+else
+    echo "Xvfb started successfully"
+fi
 
 export DISPLAY=:99.0
 export PUPPETEER_EXEC_PATH="$(which google-chrome-stable)"
